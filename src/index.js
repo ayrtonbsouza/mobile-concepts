fb33d9162b0a624a8800f472b4a7d5a750b15349
Ayrton Souza <ayrtonsouza@icloud.com>
2020-06-19 02:12:35 -0300

    understanding the differences between react and react native

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
+
+/**
+ * Não possuem valor semântico;
+ * Não possuem estilização própria
+ * View: div - footer - header - main - aside - section
+ * Text: p, span, h1, h2, h3
+ * Todos componentes por padrão "display:flex"
+ */
 
 export default function App() {
-  return <View />
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello, world</Text>
+      </View>
+    </>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#7159c1',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  title: {
+    color: '#FFF',
+    fontSize: 32,
+    fontWeight: 'bold'
+  }
+})
